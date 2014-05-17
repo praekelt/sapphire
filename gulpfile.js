df5408fc49bc6243f11f2a5e276642078803c541
justinvdm <justinvdm.0.1.0@gmail.com>
2014-05-17 14:20:52 +0200

    Add build steps for scripts using browserify and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
 var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+
+
+gulp.task('scripts', function () {
+  gulp
+    .src("./src/scripts/sapphire.js")
+    .pipe(browserify({standalone: 'sapphire'}))
+    .pipe(gulp.dest("./build"));
+});
+
+
+gulp.task('build', function () {
+  gulp.start('scripts');
+});
 
 
 gulp.task('default', function () {
+  gulp.start('build');
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
 });
