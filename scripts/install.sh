223c9d4d7bb55b142b26a162523cd573b3ff3e85
justinvdm <justinvdm.0.1.0@gmail.com>
2014-06-05 16:18:23 +0200

    Rebuild on post-merge

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
-hookfile=.git/hooks/post-commit
+commit_hook=.git/hooks/post-commit
+merge_hook=.git/hooks/post-merge
 
 if [ -d './.git' ]; then
   mkdir -p '.git/hooks'
   git update-index --assume-unchanged ./build/*
 
-  echo "#!/bin/sh" > $hookfile
-  echo "npm run-script post-commit" >> $hookfile
-  chmod +x $hookfile
+  echo "#!/bin/sh" > $commit_hook
+  echo "npm run-script build" >> $commit_hook
+  chmod +x $commit_hook
+
+  echo "#!/bin/sh" > $merge_hook
+  echo "npm run-script build" >> $merge_hook
+  chmod +x $merge_hook
 fi
