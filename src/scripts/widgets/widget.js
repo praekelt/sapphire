ab45e096efe8abe858a36f40439306b94a02323a
justinvdm <justinvdm.0.1.0@gmail.com>
2014-06-12 16:53:15 +0200

    Change base widget type's dimension properties to accessors

--- src/scripts/widgets/widget.js
+++ src/scripts/widgets/widget.js
@@ -6,15 +6,21 @@ module.exports = require('../view').extend()
   .rowspan(1)
 
   .confprop('width')
+  .set(d3.functor)
   .width(200)
 
   .confprop('height')
+  .set(d3.functor)
   .height(200)
 
   .draw(function() {
     var self = this;
 
     this.el()
-      .style('width', function() { return self.width() + 'px'; })
-      .style('height', function() { return self.height() + 'px'; });
+      .style('width', function(d, i) {
+        return self.width().call(this, d, i) + 'px';
+      })
+      .style('height', function(d, i) {
+        return self.height().call(this, d, i) + 'px';
+      });
   });
