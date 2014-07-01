ec80289a865cbc33ebfb740f28b033d6d4bee8c2
justinvdm <justinvdm.0.1.0@gmail.com>
2014-07-01 08:29:33 +0200

    Build

--- build/sapphire.js
+++ build/sapphire.js
@@ -321,10 +321,20 @@ utils.ensure = function(v, defaultval) {
 };
 
 
+utils.translate = function(x, y) {
+  return 'translate(' + x + ', ' + y + ')';
+};
+
+
 utils.ensureEl = function(el) {
   return !(el instanceof d3.selection)
     ? d3.select(el)
     : el;
+};
+
+
+utils.date = function(t) {
+  return new Date(t);
 };
 
 },{}],5:[function(_dereq_,module,exports){
@@ -374,7 +384,16 @@ exports.widget = _dereq_('./widget');
 exports.lastvalue = _dereq_('./lastvalue');
 
 },{"./lastvalue":7,"./widget":8}],7:[function(_dereq_,module,exports){
+var utils = _dereq_('../utils');
+
+
 module.exports = _dereq_('./widget').extend()
+  .prop('width')
+  .default(400)
+
+  .prop('colspan')
+  .default(2)
+
   .prop('title')
   .set(d3.functor)
   .default(function(d) { return d.title; })
@@ -391,36 +410,206 @@ module.exports = _dereq_('./widget').extend()
   .set(d3.functor)
   .default(function(d) { return d.y; })
 
-  .prop('format')
-  .default(d3.format())
+  .prop('fvalue')
+  .default(d3.format(',2s'))
+
+  .prop('fdiff')
+  .default(d3.format('+,2s'))
+
+  .prop('ftime')
+  .default(d3.time.format('%-d %b %-H:%M'))
 
   .prop('none')
   .default(0)
 
+  .prop('sparkline')
+  .prop('summary')
+
+  .init(function() {
+    this.sparkline(sparkline(this));
+    this.summary(summary(this));
+  })
+
   .enter(function(el) {
-    el.append('class', 'lastvalue')
-      .append('div')
-        .attr('class', 'last');
+    el.attr('class', 'lastvalue');
+
+    el.append('div')
+      .attr('class', 'title');
+
+    var values = el.append('div')
+      .attr('class', 'values');
+
+    values.append('div')
+      .attr('class', 'last value');
+
+    values.append('div')
+      .attr('class', 'sparkline');
+
+    values.append('div')
+      .attr('class', 'summary');
   })
 
   .draw(function(el) {
     var self = this;
+    var node = el.node();
 
-    el.select('.last')
-      .datum(function(d, i) {
-        var values = self.values().call(this, d, i);
-        return values[values.length - 1];
-      })
+    el.select('.title')
       .text(function(d, i) {
-        var v = d
-          ? self.y().call(this, d, i)
-          : self.none();
+        return self.title().call(node, d, i);
+      });
 
-          return self.format()(v);
+    var values = el.select('.values')
+      .datum(function(d, i) {
+        return self.values()
+          .call(node, d, i)
+          .map(function(d, i) {
+            return {
+              x: self.x().call(node, d, i),
+              y: self.y().call(node, d, i)
+            };
+          });
+      })
+      .attr('class', function(d) {
+        d = d.slice(-2);
+
+        d = d.length > 1
+          ? d[1].y - d[0].y
+          : 0;
+
+        if (d > 0) { return 'good values'; }
+        if (d < 0) { return 'bad values'; }
+        return 'neutral values';
+      });
+
+    values.select('.last.value')
+      .datum(function(d, i) {
+        d = d[d.length - 1];
+
+        return !d
+          ? self.none()
+          : d.y;
+      })
+      .text(this.fvalue());
+
+    values.select('.sparkline')
+      .call(this.sparkline());
+
+    values.select('.summary')
+      .call(this.summary());
+  });
+
+
+var summary = _dereq_('../view').extend()
+  .prop('lastvalue')
+
+  .init(function(lastvalue) {
+    this.lastvalue(lastvalue);
+  })
+
+  .enter(function(el) {
+    el.append('span')
+      .attr('class', 'diff');
+
+    el.append('span')
+      .attr('class', 'time');
+  })
+
+  .draw(function(el) {
+    var lastvalue = this.lastvalue();
+    if (el.datum().length < 2) { return; }
+
+    el.select('.diff')
+      .datum(function(d) {
+        d = d.slice(-2);
+        return d[1].y - d[0].y;
+      })
+      .text(lastvalue.fdiff());
+
+    el.select('.time')
+      .datum(function(d) {
+        d = d.slice(-2);
+
+        return [d[0].x, d[1].x]
+          .map(utils.date)
+          .map(lastvalue.ftime());
+      })
+      .text(function(d) {
+        return [' from', d[0], 'to', d[1]].join(' ');
       });
   });
 
-},{"./widget":8}],8:[function(_dereq_,module,exports){
+
+var sparkline = _dereq_('../view').extend()
+  .prop('lastvalue')
+
+  .prop('height').default(25)
+
+  .prop('margin').default({
+    top: 4,
+    left: 4,
+    bottom: 4,
+    right: 4 
+  })
+
+  .init(function(lastvalue) {
+    this.lastvalue(lastvalue);
+  })
+
+  .enter(function(el) {
+    var svg = el.append('svg')
+      .append('g');
+
+    svg.append('path')
+      .attr('class', 'rest path');
+
+    svg.append('path')
+      .attr('class', 'diff path');
+  })
+
+  .draw(function(el) {
+    var margin = this.margin();
+    var width = parseInt(el.style('width'));
+
+    var fx = d3.scale.linear()
+      .domain(d3.extent(el.datum(), function(d) { return d.x; }))
+      .range([0, width - (margin.left + margin.right)]);
+
+    var fy = d3.scale.linear()
+      .domain(d3.extent(el.datum(), function(d) { return d.y; }))
+      .range([this.height() - (margin.top + margin.bottom), 0]);
+
+    var line = d3.svg.line()
+      .x(function(d) { return fx(d.x); })
+      .y(function(d) { return fy(d.y); });
+
+    var svg = el.select('svg')
+      .attr('width', width)
+      .attr('height', this.height())
+      .select('g')
+        .attr('transform', utils.translate(margin.left, margin.top));
+
+    svg.select('.rest.path')
+      .attr('d', line);
+
+    svg.select('.diff.path')
+      .datum(function(d) { return d.slice(-2); })
+      .attr('d', line);
+
+    var dot = svg.selectAll('.dot')
+      .data(function(d) { return d.slice(-1); });
+
+    dot.enter().append('circle')
+      .attr('class', 'dot')
+      .attr('r', 4);
+
+    dot
+      .attr('cx', function(d) { return fx(d.x); })
+      .attr('cy', function(d) { return fy(d.y); });
+
+    dot.exit().remove();
+  });
+
+},{"../utils":4,"../view":5,"./widget":8}],8:[function(_dereq_,module,exports){
 module.exports = _dereq_('../view').extend()
   .prop('colspan')
   .default(1)
