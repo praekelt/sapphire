e510877f1e9a80f6033160c938b7b7a8a76fdb81
justinvdm <justinvdm.0.1.0@gmail.com>
2014-07-09 09:43:24 +0200

    Build

--- build/sapphire.js
+++ build/sapphire.js
@@ -100,6 +100,8 @@ module.exports = _dereq_('./view').extend()
       .row(function(d) { return d.row; })
       .colspan(function(d) { return d.colspan; })
       .rowspan(function(d) { return d.rowspan; });
+    
+    el.style('width', (grid.scale() * grid.numcols()) + 'px');
 
     var widgets = el.select('.widgets')
       .datum(widgetData);
