a892b4f696f0d72579dcf81367103954c0c8ab0d
justinvdm <justinvdm.0.1.0@gmail.com>
2014-07-16 21:53:29 +0200

    Ensure different lines widget type instances don't share the same colour scale

--- src/scripts/widgets/lines.js
+++ src/scripts/widgets/lines.js
@@ -46,7 +46,6 @@ module.exports = require('./widget').extend()
   .default(7)
 
   .prop('colors')
-  .default(d3.scale.category10())
 
   .prop('none')
   .default(0)
@@ -57,6 +56,7 @@ module.exports = require('./widget').extend()
   .init(function() {
     this.chart(chart(this));
     this.legend(legend(this));
+    this.colors(this.colors() || d3.scale.category10());
   })
 
   .enter(function(el) {
