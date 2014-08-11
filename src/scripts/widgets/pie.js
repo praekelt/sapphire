e9754f476460cf0f27e64f946af2253206a0400c
justinvdm <justinvdm.0.1.0@gmail.com>
2014-08-11 15:45:09 +0200

    Get pie widget to draw its slices

--- src/scripts/widgets/pie.js
+++ src/scripts/widgets/pie.js
@@ -1,3 +1,6 @@
+var utils = require('../utils');
+
+
 module.exports = require('./widget').extend()
   .prop('width')
   .default(400)
@@ -33,6 +36,18 @@ module.exports = require('./widget').extend()
   .set(d3.functor)
   .default(function(d) { return d.value; })
 
+  .prop('margin')
+  .default({
+    top: 20,
+    left: 20,
+    right: 20,
+    bottom: 20
+  })
+
+  .prop('innerRadius')
+  .set(d3.functor)
+  .default(function(r) { return 0.35 * r; })
+
   .init(function() {
     this.colors(d3.scale.category10());
   })
@@ -44,8 +59,24 @@ module.exports = require('./widget').extend()
     el.datum(function(d, i) {
       return {
         title: self.title().call(node, d, i),
+        metrics: self.metrics()
+          .call(node, d, i)
+          .map(metric)
       };
     });
+
+    function metric(d, i) {
+      var key = self.key()
+        .call(node, d, i)
+        .toString();
+
+      return {
+        key: key,
+        color: self.colors()(key),
+        title: self.metricTitle().call(node, d, i),
+        value: self.value().call(node, d, i)
+      };
+    }
   })
 
   .enter(function(el) {
@@ -53,11 +84,58 @@ module.exports = require('./widget').extend()
 
     el.append('div')
       .attr('class', 'title');
+
+    el.append('div')
+      .attr('class', 'chart')
+        .append('svg')
+        .append('g');
   })
 
   .draw(function(el) {
     this.normalize(el);
+    el.style('height', el.style('min-height'));
 
     el.select('.widget .title')
       .text(function(d) { return d.title; });
+
+    var chart = el.select('.chart');
+
+    var dims = utils.box()
+      .margin(this.margin())
+      .width(parseInt(chart.style('width')))
+      .height(parseInt(chart.style('height')))
+      .calc();
+
+    var radius = Math.min(dims.innerWidth, dims.innerHeight) / 2;
+
+    var svg = chart.select('svg')
+      .attr('width', dims.width)
+      .attr('height', dims.height)
+      .select('g')
+        .attr('transform', utils.translate(dims.margin.left, dims.margin.top));
+
+    var arc = d3.svg.arc()
+      .innerRadius(this.innerRadius()(radius))
+      .outerRadius(radius);
+
+    var layout = d3.layout.pie()
+      .value(function(d) { return d.value; });
+
+    var slice = svg.selectAll('.slice')
+      .data(function(d) { return layout(d.metrics); },
+            function(d) { return d.data.key; });
+
+    slice.enter().append('g')
+      .attr('class', 'slice')
+      .append('path');
+
+    slice
+      .attr('transform', utils.translate(radius, radius));
+
+    slice.select('path')
+      .attr('d', arc)
+      .style('fill', function(d) { return d.data.color; });
+
+    slice.exit()
+      .remove();
   });
--- test/widgets/pie.test.js
+++ test/widgets/pie.test.js
@@ -1,6 +1,34 @@
 describe("sapphire.widgets.pie", function() {
   var el;
   var datum;
+  
+  var helpers = {};
+
+  helpers.arc = strain()
+    .prop('margin')
+    .prop('width')
+    .prop('height')
+    .prop('innerRadius')
+    .prop('data')
+    .invoke(function(key) {
+      var dims = sapphire.utils.box()
+        .margin(this.margin())
+        .width(this.width())
+        .height(this.height())
+        .calc();
+
+      var arc = d3.svg.arc()
+        .innerRadius(this.innerRadius())
+        .outerRadius(Math.min(dims.innerWidth, dims.innerHeight) / 2);
+
+      var layout = d3.layout.pie()
+        .value(function(d) { return d.value; });
+
+      var d = layout(this.data())
+        .filter(function(d){ return d.data.key === key; })[0];
+
+      return arc(d);
+    });
 
   beforeEach(function() {
     el = d3.select('body')
@@ -16,7 +44,11 @@ describe("sapphire.widgets.pie", function() {
       }, {
         key: 'bar',
         title: 'Total Bar',
-        value: 4000000
+        value: 2000000
+      }, {
+        key: 'baz',
+        title: 'Total Baz',
+        value: 3000000
       }]
     };
   });
@@ -46,5 +78,143 @@ describe("sapphire.widgets.pie", function() {
     title = el.selectAll('.widget > .title');
     expect(title.size()).to.equal(1);
     expect(title.text()).to.equal(datum.title);
+  });
+
+  it("should show its slices", function() {
+    var pie = sapphire.widgets.pie()
+      .key(function(d) { return d.key; });
+
+    expect(el.html()).to.be.empty;
+
+    el.datum(datum)
+      .call(pie);
+
+    datum.metrics[0].key = 'foo';
+    datum.metrics[1].key = 'bar';
+    datum.metrics[2].key = 'baz';
+
+    var slice = el.selectAll('.slice');
+    expect(slice.size()).to.equal(3);
+
+    datum.metrics[0].key = 'foo';
+    datum.metrics[1].key = 'ham';
+    datum.metrics[2].key = 'baz';
+
+    el.datum(datum)
+      .call(pie);
+
+    slice = el.selectAll('.slice');
+    expect(slice.size()).to.equal(3);
+  });
+
+  it("should size its slices according to their values", function() {
+    var pie = sapphire.widgets.pie()
+      .width(300)
+      .height(400)
+      .margin({
+        top: 20,
+        left: 20,
+        right: 20,
+        bottom: 20
+      })
+      .innerRadius(15)
+      .key(function(d) { return d.key; });
+
+    var arc = helpers.arc()
+      .margin({
+        top: 20,
+        left: 20,
+        right: 20,
+        bottom: 20
+      })
+      .width(300)
+      .height(400)
+      .innerRadius(15);
+
+    expect(el.html()).to.be.empty;
+
+    el.datum(datum)
+      .call(pie);
+
+    datum.metrics[0].key = 'foo';
+    datum.metrics[1].key = 'bar';
+    datum.metrics[2].key = 'baz';
+
+    datum.metrics[0].value = 4000000;
+    datum.metrics[1].value = 2000000;
+    datum.metrics[2].value = 3000000;
+
+    arc.data(datum.metrics);
+    expect(d('foo')).to.equal(arc('foo'));
+    expect(d('bar')).to.equal(arc('bar'));
+    expect(d('baz')).to.equal(arc('baz'));
+
+    datum.metrics[0].key = 'foo';
+    datum.metrics[1].key = 'ham';
+    datum.metrics[2].key = 'baz';
+
+    datum.metrics[0].value = 5000000;
+    datum.metrics[1].value = 6000000;
+    datum.metrics[2].value = 7000000;
+
+    el.datum(datum)
+      .call(pie);
+
+    arc.data(datum.metrics);
+    expect(d('foo')).to.equal(arc('foo'));
+    expect(d('ham')).to.equal(arc('ham'));
+    expect(d('baz')).to.equal(arc('baz'));
+
+    function d(key) {
+      return el.selectAll('.slice path')
+        .filter(function(d) { return d.data.key === key; })
+        .attr('d');
+    }
+  });
+
+  it("should colour its slices according to their keys", function() {
+    var pie = sapphire.widgets.pie()
+      .width(300)
+      .height(400)
+      .margin({
+        top: 20,
+        left: 20,
+        right: 20,
+        bottom: 20
+      })
+      .innerRadius(15)
+      .key(function(d) { return d.key; });
+
+    var colors = pie.colors();
+
+    expect(el.html()).to.be.empty;
+
+    el.datum(datum)
+      .call(pie);
+
+    datum.metrics[0].key = 'foo';
+    datum.metrics[1].key = 'bar';
+    datum.metrics[2].key = 'baz';
+
+    expect(fill('foo')).to.equal(colors('foo'));
+    expect(fill('bar')).to.equal(colors('bar'));
+    expect(fill('baz')).to.equal(colors('baz'));
+
+    datum.metrics[0].key = 'foo';
+    datum.metrics[1].key = 'ham';
+    datum.metrics[2].key = 'baz';
+
+    el.datum(datum)
+      .call(pie);
+
+    expect(fill('foo')).to.equal(colors('foo'));
+    expect(fill('ham')).to.equal(colors('ham'));
+    expect(fill('baz')).to.equal(colors('baz'));
+
+    function fill(key) {
+      return el.selectAll('.slice path')
+        .filter(function(d) { return d.data.key === key; })
+        .style('fill');
+    }
   });
 });
