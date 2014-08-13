2d4089eb6922e6239587bcbc5653f7a3cf1f57f0
justinvdm <justinvdm.0.1.0@gmail.com>
2014-08-13 12:45:30 +0200

    Build

--- build/sapphire.js
+++ build/sapphire.js
@@ -656,13 +656,14 @@ module.exports = _dereq_('./widget').extend()
       .call(axis);
   });
 
-},{"../utils":4,"./widget":10}],7:[function(_dereq_,module,exports){
-exports.widget = _dereq_('./widget');
-exports.lines = _dereq_('./lines');
-exports.last = _dereq_('./last');
+},{"../utils":4,"./widget":11}],7:[function(_dereq_,module,exports){
+exports.pie = _dereq_('./pie');
 exports.bars = _dereq_('./bars');
+exports.last = _dereq_('./last');
+exports.lines = _dereq_('./lines');
+exports.widget = _dereq_('./widget');
 
-},{"./bars":6,"./last":8,"./lines":9,"./widget":10}],8:[function(_dereq_,module,exports){
+},{"./bars":6,"./last":8,"./lines":9,"./pie":10,"./widget":11}],8:[function(_dereq_,module,exports){
 var utils = _dereq_('../utils');
 
 
@@ -910,7 +911,7 @@ var sparkline = _dereq_('../view').extend()
     dot.exit().remove();
   });
 
-},{"../utils":4,"../view":5,"./widget":10}],9:[function(_dereq_,module,exports){
+},{"../utils":4,"../view":5,"./widget":11}],9:[function(_dereq_,module,exports){
 var utils = _dereq_('../utils');
 
 
@@ -1229,7 +1230,231 @@ var legend = _dereq_('../view').extend()
       .remove();
   });
 
-},{"../utils":4,"../view":5,"./widget":10}],10:[function(_dereq_,module,exports){
+},{"../utils":4,"../view":5,"./widget":11}],10:[function(_dereq_,module,exports){
+var utils = _dereq_('../utils');
+
+
+module.exports = _dereq_('./widget').extend()
+  .prop('width')
+  .default(400)
+
+  .prop('colspan')
+  .default(4)
+
+  .prop('rowspan')
+  .default(4)
+
+  .prop('height')
+  .default(200)
+
+  .prop('colors')
+
+  .prop('title')
+  .set(d3.functor)
+  .default(function(d) { return d.title; })
+
+  .prop('metrics')
+  .set(d3.functor)
+  .default(function(d) { return d.metrics; })
+
+  .prop('key')
+  .set(d3.functor)
+  .default(function(d, i) { return i; })
+
+  .prop('metricTitle')
+  .set(d3.functor)
+  .default(function(d) { return d.title; })
+
+  .prop('value')
+  .set(d3.functor)
+  .default(function(d) { return d.value; })
+
+  .prop('margin')
+  .default({
+    top: 20,
+    left: 20,
+    right: 20,
+    bottom: 20
+  })
+
+  .prop('innerRadius')
+  .set(d3.functor)
+  .default(function(r) { return 0.35 * r; })
+
+  .prop('valueFormat')
+  .default(d3.format(',2s'))
+
+  .prop('percentFormat')
+  .default(d3.format('.0%'))
+
+  .init(function() {
+    this.colors(d3.scale.category10());
+  })
+
+  .meth(function normalize(el) {
+    var self = this;
+    var node = el.node();
+
+    el.datum(function(d, i) {
+      return {
+        title: self.title().call(node, d, i),
+        metrics: self.metrics()
+          .call(node, d, i)
+          .map(metric)
+      };
+    });
+
+    function metric(d, i) {
+      var key = self.key()
+        .call(node, d, i)
+        .toString();
+
+      return {
+        key: key,
+        color: self.colors()(key),
+        title: self.metricTitle().call(node, d, i),
+        value: self.value().call(node, d, i)
+      };
+    }
+
+    var sum = d3.sum(el.datum().metrics, function(d) { return d.value; });
+    el.datum().metrics.forEach(function(d) { d.percent = d.value / sum; });
+  })
+
+  .enter(function(el) {
+    el.attr('class', 'pie widget');
+
+    el.append('div')
+      .attr('class', 'title');
+
+    el.append('div')
+      .attr('class', 'chart');
+
+    el.append('div')
+      .attr('class', 'legend');
+  })
+
+  .draw(function(el) {
+    this.normalize(el);
+
+    el.select('.widget .title')
+      .text(function(d) { return d.title; });
+
+    el.select('.legend')
+      .call(legend(this));
+
+    el.select('.chart')
+      .style('height', el.style('min-height'))
+      .call(chart(this));
+  });
+
+
+var chart = _dereq_('../view').extend()
+  .prop('widget')
+
+  .init(function(widget) {
+    this.widget(widget);
+  })
+
+  .enter(function(el) {
+    el.append('svg')
+      .append('g');
+  })
+
+  .draw(function(el) {
+    var dims = utils.box()
+      .margin(this.widget().margin())
+      .width(parseInt(el.style('width')))
+      .height(parseInt(el.style('height')))
+      .calc();
+
+    var radius = Math.min(dims.innerWidth, dims.innerHeight) / 2;
+
+    var svg = el.select('svg')
+      .attr('width', dims.width)
+      .attr('height', dims.height)
+      .select('g')
+        .attr('transform', utils.translate(dims.margin.left, dims.margin.top));
+
+    var arc = d3.svg.arc()
+      .innerRadius(this.widget().innerRadius()(radius))
+      .outerRadius(radius);
+
+    var layout = d3.layout.pie()
+      .value(function(d) { return d.value; });
+
+    var slice = svg.selectAll('.slice')
+      .data(function(d) { return layout(d.metrics); },
+            function(d) { return d.data.key; });
+
+    slice.enter().append('g')
+      .attr('class', 'slice')
+      .append('path');
+
+    slice
+      .attr('transform', utils.translate(radius, radius));
+
+    slice.select('path')
+      .attr('d', arc)
+      .style('fill', function(d) { return d.data.color; });
+
+    slice.exit()
+      .remove();
+  });
+
+
+var legend = _dereq_('../view').extend()
+  .prop('widget')
+
+  .init(function(widget) {
+    this.widget(widget);
+  })
+
+  .enter(function(el) {
+    el.append('table')
+      .attr('class', 'table');
+  })
+
+  .draw(function(el) {
+    var valueFormat = this.widget().valueFormat();
+    var percentFormat = this.widget().percentFormat();
+
+    var metric = el.select('.table').selectAll('.metric')
+      .data(function(d) { return d.metrics; },
+            function(d) { return d.key; });
+
+    var enterMetric = metric.enter().append('tr')
+      .attr('class', 'metric');
+
+    enterMetric.append('td')
+      .attr('class', 'swatch');
+
+    enterMetric.append('td')
+      .attr('class', 'title');
+
+    enterMetric.append('td')
+      .attr('class', 'percent');
+
+    enterMetric.append('td')
+      .attr('class', 'value');
+
+    metric.select('.swatch')
+      .style('background', function(d) { return d.color; });
+
+    metric.select('.title')
+      .text(function(d) { return d.title; });
+
+    metric.select('.percent')
+      .text(function(d) { return percentFormat(d.percent); });
+
+    metric.select('.value')
+      .text(function(d) { return valueFormat(d.value); });
+
+    metric.exit()
+      .remove();
+  });
+
+},{"../utils":4,"../view":5,"./widget":11}],11:[function(_dereq_,module,exports){
 module.exports = _dereq_('../view').extend()
   .prop('standalone')
   .default(true)
