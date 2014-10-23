7d5c421fc4de12229087b94f78aea5981a87b89f
justinvdm <justinvdm.0.1.0@gmail.com>
2014-10-23 14:30:50 +0200

    Remove the enter and draw magic being done in base view now that we don't need it anymore

--- src/scripts/view.js
+++ src/scripts/view.js
@@ -1,50 +1,18 @@
 module.exports = strain()
   .static('draw', function(fn) {
-    this.meth('_draw_', safeDraw(fn));
+    this.meth('_draw_', fn);
   })
   .draw(function() {})
-
-  .static('enter', function(fn) {
-    this.meth('_enter_', safeDraw(fn));
-  })
-  .enter(function() {})
 
   .meth('draw', function(el) {
     el = sapphire.utils.ensureEl(el);
 
-    if (el.node() && !el.node().hasChildNodes()) {
-      this.enter.apply(this, arguments);
-    }
-
-    var parent = this._type_._super_.prototype;
-    if ('_draw_' in parent) {
-      parent._draw_.apply(this, arguments);
-    }
-
+    var datum;
+    if (el.node()) datum = el.datum();
     this._draw_.apply(this, arguments);
-  })
-
-  .meth('enter', function(el) {
-    el = sapphire.utils.ensureEl(el);
-
-    var parent = this._type_._super_.prototype;
-    if ('_enter_' in parent) {
-      parent._enter_.apply(this, arguments);
-    }
-
-    this._enter_.apply(this, arguments);
+    if (typeof datum != 'undefined') el.datum(datum);
   })
 
   .invoke(function() {
     return this.draw.apply(this, arguments);
   });
-
-
-function safeDraw(fn) {
-  return function(el) {
-    var datum;
-    if (el.node()) datum = el.datum();
-    fn.apply(this, arguments);
-    if (typeof datum != 'undefined') el.datum(datum);
-  };
-}
